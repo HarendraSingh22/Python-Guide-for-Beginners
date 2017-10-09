76032ec752b4a89f2a07001b50455e8a921a9a4c
Jill Cates <cates.jill@gmail.com>
2017-10-08 22:52:34 -0700

    Update naming of factorial.py

--- Code/factorial.py
+++ Code/factorial.py
@@ -1,16 +1,7 @@
+x = input()
+ans = 1
+while(x>0):
+    ans*=x
+    x-=1
 
-#program to find the factorial of a number provided by a user
-
-if __name__ == "__main__":
-    number = int(raw_input("Enter a number to get its factorial: "))
-    factorial = 1
-
-    if number < 0:
-        print("Sorry, factorial doesn't exist for negative numbers!")
-    elif number == 0:
-        print("The factorial of 0 is 1")
-    else:
-        for i in range(1, number + 1):
-            factorial = factorial * i
-        print "The factorial of %r is %r" % (number, factorial)
-    
+print ans
