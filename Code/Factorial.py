23aa0dd5b5b1160111a371ebd0835225f18feaca
Jatin <jatinrohilla69@gmail.com>
2017-10-09 14:56:38 +0530

    converted factorial to function

--- Code/Factorial.py
+++ Code/Factorial.py
@@ -1,7 +1,15 @@
-x = input()
-ans = 1
-while(x>0):
-    ans*=x
-    x-=1
 
-print ans
+#program to find the factorial of a number provided by a user
+
+def fact(number):
+    factorial = 1
+    for i in range(1, number + 1):
+        factorial = factorial * i
+    return factorial
+
+if __name__ == "__main__":
+    number = int(input("Enter a number to get its factorial: "))
+    if number < 0:
+        print("Sorry, factorial doesn't exist for negative numbers!")
+    result = fact(number)
+    print("factorial of",number,"is :",result)
