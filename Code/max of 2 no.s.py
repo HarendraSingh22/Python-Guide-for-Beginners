6b61beca2c494e95f1e003d48cfa188174f3730a
DamiyanVC <DamiyanVC@gmail.com>
2017-10-10 00:51:25 +0530

    add a program for max of 2 numbers

---
+++ Code/max of 2 no.s.py
@@ -0,0 +1,16 @@
+# program for max of 2 no.s
+
+first num=int(input())
+second num=int(input())
+
+# using builtin functions
+
+max(first num,second num)
+print(max)
+
+# without using functions
+
+if first num>second num:
+      print(first num"is greatest")
+else:
+      print(second num"is greatest")
