6971533c9c1b875463fad67ccc953c1dbc8e681f
Siddharth Aggarwal <siddhu.15798@gmail.com>
2017-10-09 22:05:25 +0530

    Python programme for Fibonacci Series

---
+++ Code/FibonacciSeries.py
@@ -0,0 +1,24 @@
+# Program to display the Fibonacci series up to nth term where n is provided by the user
+
+nterms = int(input("Enter the number of terms upto which you want to print the series: "))
+
+# first two terms
+n1 = 0
+n2 = 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence upto",nterms,":")
+   while count < nterms:
+       print(n1,end=' , ')
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
