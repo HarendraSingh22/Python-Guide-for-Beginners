c12af3a3dbb697b872c375ba841b6a328bd2f2d6
yeshijinsel <yeshijinsel@gmail.com>
2017-10-12 22:22:02 +0530

    Divide the two numbers in Python

---
+++ Code/Yeshidivide.py
@@ -0,0 +1,10 @@
+Nuum1 = input("Enter first number: ")
+Num2 = input("Enter second number: ")
+
+
+if num2 == 0:
+    print 'Denominator cannot be 0'
+
+else:
+    Division = float(num1) / float(num2)
+    print Division
