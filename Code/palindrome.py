077ae56220df1d1be8c02085a127834230ea4973
Cam <CameronD.Barts@gmail.com>
2019-08-20 20:14:24 -0400

    Added comments

--- Code/palindrome.py
+++ Code/palindrome.py
@@ -3,15 +3,24 @@
 """
 Created on Tue Oct 31 17:23:49 2017
 
-@author: TheKingOfShade
+@author: cam-barts
 """
 import argparse
-
+# Using argparse, which is a argument parsing module from the standard library, to take input from the user
+# https://docs.python.org/2/library/argparse.html
 parser = argparse.ArgumentParser()
+
+# Create an agrument, named 'String' and will be accessed as args.String. It will be of string type, and the help block with say "String to check palindrome-ness"
+# $python palindrome.py -h
+# usage: palindrome.py [-h] <string>
+#
+# positional arguments:
+#  <sring>           String to check palindrome-ness
 parser.add_argument("String", type=str, help= "String to check palindrome-ness")
 args = parser.parse_args()
 String = args.String
 
+# Adding [::-1] reverses a string in python2
 StringReverse = String[::-1]
 
 if __name__ == '__main__':
@@ -19,4 +28,3 @@ if __name__ == '__main__':
         print "String is a palindrome!"
     else:
         print "String is not a palindrome!"
-
