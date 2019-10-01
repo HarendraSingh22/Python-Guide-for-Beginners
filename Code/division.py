597f6e41c2c6d21c80da40ec7f5aa71239efd80e
Satyam Bharti <44036099+satyambharti171@users.noreply.github.com>
2019-10-01 16:02:31 +0530

    Update Division

--- Code/division.py
+++ Code/division.py
@@ -1,8 +1,1 @@
-
-firstnum = int(input("First number: "))
-secondnum = int(input("Second number: "))
-
-if secondnum == 0:
-	print("Division by zero illegal.")
-else:
-	print("The result is " + str(firstnum/secondnum))
+Print(input("Enter First Number: ")/input("Enter Second Number"))
