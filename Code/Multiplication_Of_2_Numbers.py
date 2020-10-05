6d9b1d3eb908da0854a7e71a6d1e3c5a261444f5
TheCoder1232 <72138846+TheCoder1232@users.noreply.github.com>
2020-10-05 10:10:12 +0530

    improved code
    added while loop for continuation

--- Code/Multiplication_Of_2_Numbers.py
+++ Code/Multiplication_Of_2_Numbers.py
@@ -1,3 +1,18 @@
-a=input("Enter a number -->")
-b=input("Enter a number -->")
-print a*b
+
+while True:
+  
+  print("(enter 'q' to quit anytime.)")
+  
+  a = input("Enter a number -->")
+  if a == 'q':
+    break
+  else:
+    a = int(a)
+    
+  b=input("Enter a number -->")
+  if b == 'q':
+    break
+  else:
+    b = int(b)
+    
+  print("The product is --> {}".format(a*b))
