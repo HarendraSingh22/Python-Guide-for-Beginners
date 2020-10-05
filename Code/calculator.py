1c028e44dc287be0bca07b23c682cd196421bd53
TheCoder1232 <72138846+TheCoder1232@users.noreply.github.com>
2020-10-05 09:47:34 +0530

    improved code
    added while loop and try - except block

--- Code/calculator.py
+++ Code/calculator.py
@@ -1,18 +1,25 @@
-# This function adds two numbers 
+# This function adds two numbers
 def add(x, y):
-   return x + y
+    return x + y
 
-# This function subtracts two numbers 
+# This function subtracts two numbers
+
+
 def subtract(x, y):
-   return x - y
+    return x - y
 
 # This function multiplies two numbers
+
+
 def multiply(x, y):
-   return x * y
+    return x * y
 
 # This function divides two numbers
+
+
 def divide(x, y):
-   return x / y
+    return x / y
+
 
 print("Select operation.")
 print("1.Add")
@@ -20,22 +27,40 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-# Take input from the user 
-choice = input("Enter choice(1/2/3/4):")
+# Take input from the user
+while True:
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+    try:
+        print("(enter 'q' to quit anytime.)")
+        choice = input("Enter choice(1/2/3/4):")
+        if choice == 'q':
+            break
 
-if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
+        num1 = input("Enter first number: ")
+        if num1 == 'q':
+            break
+        else:
+            num1 = int(num1)
 
-elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
+        num2 = input("Enter second number: ")
+        if num2 == 'q':
+            break
+        else:
+            num2 = int(num2)
 
-elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
 
-elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
-else:
-   print("Invalid input")
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        else:
+            print("Invalid Input")
+
+    except ValueError:
+        print("Invalid Input")
