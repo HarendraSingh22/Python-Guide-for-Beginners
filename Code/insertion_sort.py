2c5ad2fdec754a0f02789bd2d1df4fd9a5e822ce
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-10-20 10:58:14 +0330

    insertion_sort.py was completed (#173)

--- Code/insertion_sort.py
+++ Code/insertion_sort.py
@@ -4,7 +4,29 @@
 # contact me pls ( email: ebsa491@gmail.com )
 
 def main():
-    pass
+
+    array = [5, 7, 3, 9, 1, 2, 5]
+    sorting(array)
+
+    for element in array:
+        print(f"{element}")
+
+def sorting(array):
+    """
+    This function sorts the array (insertion sort).
+    :param array: The array that you want to sort.
+    """
+    for i in range(1, len(array)):
+
+        key = array[i]
+
+        j = i - 1
+
+        while j >= 0 and key < array[j]:
+                array[j + 1] = array[j]
+                j -= 1
+
+        array[j + 1] = key
 
 if __name__ == '__main__':
     main()
