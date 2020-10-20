5f0b13eb51081e7698ffcf1997abe2ba1c9d2d7e
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-10-20 10:26:49 +0330

    insertion_sort.py was added

---
+++ Code/insertion_sort.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Created by Sam Ebison ( https://github.com/ebsa491 )
+# If you have found any important bug or vulnerability,
+# contact me pls ( email: ebsa491@gmail.com )
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+  
