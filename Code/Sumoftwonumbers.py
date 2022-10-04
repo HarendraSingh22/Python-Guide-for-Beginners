2b3236759dc3892001d2098faf7a9641681a501f
JANHVI SINGH <103685692+Janhvisingh21@users.noreply.github.com>
2022-10-04 10:40:17 +0530

    Sumoftwonumbers.py

---
+++ Code/Sumoftwonumbers.py
@@ -0,0 +1,9 @@
+#python program to add two numbers
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)
+Footer
+© 2022 GitHub, Inc.
